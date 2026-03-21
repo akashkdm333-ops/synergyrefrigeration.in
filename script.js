da4af5bcb407aff9479ebaf2884f53933ea07124
AKASH KADAM <akashkdm.333@gmail.com>
2026-03-21 15:38:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function openPopup(product){
+  var phone = "918490022110";
+  var message = "Hello, I want price for " + product;
+  window.open("https://wa.me/" + phone + "?text=" + encodeURIComponent(message));
+}
+
+function scrollToProducts(){
+  document.getElementById("products").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
 console.log("Synergy site loaded");
